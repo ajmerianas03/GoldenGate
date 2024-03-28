bb0f40322026d67f58b76b2f1390a7553751fd45
Anas Ajmeri <ajmerianas03@gmail.com>
2024-03-28 17:16:51 +0530

    modifed

--- src/main/java/com/GoldenGate/GoldenGate/config/ApplicationConfig.java
+++ src/main/java/com/GoldenGate/GoldenGate/config/ApplicationConfig.java
@@ -4,6 +4,9 @@ import com.GoldenGate.GoldenGate.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,6 +23,13 @@ public class ApplicationConfig  {
         return username -> repository.findByEmail(username)
                 .orElseThrow(() ->new UsernameNotFoundException("user not found"));
 
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService();
+        return null;
     }
 
 }
--- src/main/java/com/GoldenGate/GoldenGate/config/SecurityConfiguration.java
+++ src/main/java/com/GoldenGate/GoldenGate/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.GoldenGate.GoldenGate.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
